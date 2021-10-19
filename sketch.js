cd0396a0f3d8e13e4d49420ac169dcc891d34c4a
MichaelHurtado <mhurtado@utec.edu.pe>
2021-10-19 16:07:04 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  var canvas=createCanvas(640, 480);
+  //canvas.parent('lienzo');
+  background(0);
+}
+
+function draw() {
+  fill(random(255),random(255),random(255));
+  textSize(random(30));
+  text("hola a todos",random(640),random(480));
+}
+function mouseClicked(){
+  background(0);
+}
